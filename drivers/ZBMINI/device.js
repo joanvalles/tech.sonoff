75a65ca2ffeea483ccd9770990e7a5277769b67c
joanvalles <50715754+joanvalles@users.noreply.github.com>
2024-11-17 22:37:13 +0100

    Update ZBMINI driver to add pwr on ctrl state

--- drivers/ZBMINI/device.js
+++ drivers/ZBMINI/device.js
@@ -2,17 +2,40 @@
 
 const Homey = require('homey');
 const { ZigBeeDevice } = require('homey-zigbeedriver');
-const { CLUSTER } = require('zigbee-clusters');
+const { Cluster, CLUSTER } = require('zigbee-clusters');
+
+// Import custom Sonoff clusters
+const SonoffSpecificOnOffCluster = require('../../lib/SonoffSpecificOnOffCluster');
+const SonoffSpecificOnOffSwitchCluster = require("../../lib/SonoffSpecificOnOffSwitchCluster");
+Cluster.addCluster(SonoffSpecificOnOffCluster);
+Cluster.addCluster(SonoffSpecificOnOffSwitchCluster);
 
 class ZBMINI extends ZigBeeDevice {
-	
-	async onNodeInit({ zclNode }) {
+    
+    async onNodeInit({ zclNode }) {
+        if (this.hasCapability('onoff')) {
+            this.registerCapability('onoff', CLUSTER.ON_OFF, {
+            });
+        }
+    }
 
-        if (this.hasCapability('onoff')) this.registerCapability('onoff', CLUSTER.ON_OFF, {
-		});
+    async onSettings(settingsEvent) {
+        const newSettings = settingsEvent.newSettings;
+        const changedKeys = settingsEvent.changedKeys;
 
-	}
-	
+        // Handle power loss behavior setting
+        if (changedKeys.includes('powerOnCtrl_state')) {
+            try {
+                const powerOnCtrlstate = await this.zclNode.endpoints[1].clusters.onOff.readAttributes(['powerOnCtrl']);
+                await this.zclNode.endpoints[1].clusters.onOff.writeAttributes({
+                    powerOnCtrl: newSettings.powerOnCtrl_state
+                }); // default: On (On, Off, 255 = Recover)
+                this.log("Power On Control supported by device");
+            } catch (error) {
+                this.log("This device does not support Power On Control");
+            }
+        }
+    }
 }
 
 module.exports = ZBMINI;
